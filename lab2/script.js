fef17a3b30679b70c2c915a202ef90d13b255268
Дима Трундаев <Dtrund2005@gmail.com>
2025-02-21 10:36:37 +0300

    add login to lab2, get rid of rubbish

--- lab2/script.js
+++ lab2/script.js
@@ -1,127 +1,105 @@
-// файл script.js
-window.onload = function(){ 
+window.onload = function () {
 
     let a = ''
     let b = ''
     let expressionResult = ''
     let selectedOperation = null
-    let theme = "black"
-    document.body.style.backgroundColor = "#4B7F52"
-    document.getElementById("calculator-body").style.backgroundColor = "#c9ffe2"
-    
-    // окно вывода результата
+
     outputElement = document.getElementById("result")
-    
-    // список объектов кнопок циферблата (id которых начинается с btn_digit_)
-    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
-    
+
+    digitButtons = document.querySelectorAll('[id ^= "btn-digit-"]')
+
     function onDigitButtonClicked(digit) {
         if (!selectedOperation) {
-            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) {
                 a += digit
             }
             outputElement.innerHTML = a
         } else {
-            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) {
                 b += digit
-                outputElement.innerHTML = b        
+                outputElement.innerHTML = b
             }
         }
     }
-    
-    // устанавка колбек-функций на кнопки циферблата по событию нажатия
+
     digitButtons.forEach(button => {
-        button.onclick = function() {
+        button.onclick = function () {
             const digitValue = button.innerHTML
             onDigitButtonClicked(digitValue)
         }
     });
-    
-    // установка колбек-функций для кнопок операций
-    document.getElementById("btn_op_mult").onclick = function() { 
+
+    document.getElementById("btn-op-mult").onclick = function () {
         if (a === '') return
         selectedOperation = 'x'
     }
-    document.getElementById("btn_op_plus").onclick = function() { 
+    document.getElementById("btn-op-plus").onclick = function () {
         if (a === '') return
         selectedOperation = '+'
     }
-    document.getElementById("btn_op_minus").onclick = function() { 
+    document.getElementById("btn-op-minus").onclick = function () {
         if (a === '') return
         selectedOperation = '-'
     }
-    document.getElementById("btn_op_div").onclick = function() { 
+    document.getElementById("btn-op-div").onclick = function () {
         if (a === '') return
         selectedOperation = '/'
     }
-    document.getElementById("btn_op_percent").onclick = function(){
+    document.getElementById("btn-op-percent").onclick = function () {
         if (a === '') return
         selectedOperation = '%'
     }
-    document.getElementById("btn_op_sign").onclick = function() {
+    document.getElementById("btn-op-sign").onclick = function () {
         if (a === '') return
         a = -a
         outputElement.innerHTML = a.toString();
     }
-    document.getElementById("btn_op_back").onclick = function() {
+    document.getElementById("btn-op-back").onclick = function () {
         a.toString()
         if (a === '') return;
         a = a.slice(0, -1);
         if (a === '')
-        a = '0';
-    outputElement.innerHTML = a.toString();
+            a = '0';
+        outputElement.innerHTML = a.toString();
     }
-    document.getElementById("btn_op_sqrt").onclick = function() {
+    document.getElementById("btn-op-sqrt").onclick = function () {
         if (a === '') return
         a = Math.sqrt(a)
         outputElement.innerHTML = a.toString()
     }
-    document.getElementById("btn_op_2grade").onclick = function() {
+    document.getElementById("btn-op-2grade").onclick = function () {
         if (a === '') return
         a = a ** 2
         outputElement.innerHTML = a.toString();
     }
-    document.getElementById("btn_op_factorial").onclick = function() {
+    document.getElementById("btn-op-factorial").onclick = function () {
         if (a === '') return
-        let rval=1;
+        let rval = 1;
         for (let i = 2; i <= a; i++)
             rval = rval * i;
         a = rval
         outputElement.innerHTML = a.toString();
     }
-    document.getElementById("btn_op_addthousand").onclick = function() {
+    document.getElementById("btn-op-addthousand").onclick = function () {
         if (a === '') return
         a = a * 1000
         outputElement.innerHTML = a.toString();
     }
 
-    document.getElementById("switch").onclick = function(){
-        if (theme === "black") {
-            document.body.style.backgroundColor = "#D6FFB7"
-            document.getElementById("calculator-body").style.backgroundColor = "#7E7F9A"
-            theme = "white"
-        } else {
-            document.body.style.backgroundColor = "#4B7F52"
-            document.getElementById("calculator-body").style.backgroundColor = "#c9ffe2"
-            theme = "black"
-        }
-    }
-
-    // кнопка очищения
-    document.getElementById("btn_op_clear").onclick = function() { 
+    document.getElementById("btn-op-clear").onclick = function () {
         a = ''
         b = ''
         selectedOperation = ''
         expressionResult = ''
         outputElement.innerHTML = 0
     }
-    
-    // кнопка расчёта результата
-    document.getElementById("btn_op_equal").onclick = function() { 
+
+    document.getElementById("btn-op-equal").onclick = function () {
         if (a === '' || b === '' || !selectedOperation)
             return
-            
-        switch(selectedOperation) { 
+
+        switch (selectedOperation) {
             case 'x':
                 expressionResult = (+a) * (+b)
                 break;
@@ -138,9 +116,28 @@ window.onload = function(){
                 expressionResult = (+a) % (+b)
                 break;
         }
-        
+
         a = expressionResult.toString()
-    
-        outputElement.innerHTML = a
+
+        outputElement.innerHTML = a;
     }
-    };
+
+    document.getElementById("blur").style.display = "none";
+
+    function showLogin() {
+        document.getElementById("blur").style.display = "flex";
+        document.getElementById("email").focus();
+    }
+
+    function hideLogin() {
+        document.getElementById("blur").style.display = "none";
+    }
+
+    document.getElementById("btn-login").addEventListener("click", showLogin);
+
+    document.getElementById("blur").addEventListener("click", hideLogin);
+
+    document.getElementById("login-popup").addEventListener("click", function (e) {
+        e.stopPropagation();
+    });
+};
